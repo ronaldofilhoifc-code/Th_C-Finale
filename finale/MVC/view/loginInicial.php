3809255fe18b18a3e0164ca282abcbf6143e8800
ronaldofilhoifc-code <ronaldo.filho.ifc@gmail.com>
2026-08-20 16:40:40 -0300

    responsividade da tela inicial re-adaptada

--- finale/MVC/view/loginInicial.php
+++ finale/MVC/view/loginInicial.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -19,75 +19,80 @@ session_start();
   <!-- css do site-->
   <link rel="stylesheet" href="../../assets/css/style.css">
   <link rel="stylesheet" href="../../assets/css/header.css">
+  <link rel="stylesheet" href="../../assets/css/paginaEstatica.css">
 </head>
 
 <body>
 
-  <div class="container-fluid header-sticky">
-        <div class="row">
-            <div class="col-md-12 a-boot">
-                <div class="talvez-logo" id="alvo">
-
-                <input id="errado" type="hidden" value="<?php  
-                echo $_SESSION["checkCorreto"];
-                ?>">
-
-                </div>
-            </div>
-        </div>
-    </div>
-
   <div class="container-fluid">
     <div class="row">
-
-      <div class="col-md-12 chapter-boot form-boot" id="oNormal">
-        <div class="chapter-container formulario">
-
-          <form action="../controller/login.php" method="post" class="wrapper">
-            <div class="chapter-extremidade texto">
-              <p class="titulo">Olá Entusiasta!</p>
-              <p class="linhafina">Entre em sua conta para ver o Finale.</p>
-            </div>
-            <div class="chapter-conteudo">
-
-              <div class="label-holder">
-                <p class="label">Usuário / Nome do Entusiasta: </p>
-              </div>
-              <div class="input-holder">
-                <input type="text" class="inputBasico" name="nome-usuario">
-              </div>
-              <div class="label-holder">
-                <p class="label">Senha: </p>
-              </div>
-              <div class="input-holder">
-                <input type="text" class="inputBasico" name="senha">
-              </div>
-              <div class="input-holder">
-                <div class="checkbox-holder">
-                  <input type="checkbox" class="inputBasicoCheck" name="cookie">
-                </div>
-                <div class="remember-holder">
-                  Manter-se conectado
-                </div>
-              </div>
-
-
-            </div>
-            <div class="chapter-extremidade">
-              <button>Registrar-se</button> <button type="submit">Entrar</button>
-            </div>
-
-          </form>
-        </div>
+      <div class="col-md-12 est-10">
+        <input id="errado" type="hidden" value="<?php
+        echo $_SESSION["checkCorreto"];
+        ?>">
       </div>
-
     </div>
-
   </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 est-80 organiza-info">
 
-  <?php
-  include_once("incluiveis/footer.php");
-  ?>
+        <div class="container-formulario" id="oNormal">
+          <div class="chapter-container formulario">
+
+            <form action="../controller/login.php" method="post" class="wrapper">
+              <div class="chapter-extremidade texto">
+                <p class="titulo">Olá Entusiasta!</p>
+                <p class="linhafina">Entre em sua conta para ver o Finale.</p>
+              </div>
+              <div class="chapter-conteudo">
+
+                <div class="label-holder">
+                  <p class="label">Usuário / Nome do Entusiasta: </p>
+                </div>
+                <div class="input-holder">
+                  <input type="text" class="inputBasico" name="nome-usuario">
+                </div>
+                <div class="label-holder">
+                  <p class="label">Senha: </p>
+                </div>
+                <div class="input-holder">
+                  <input type="text" class="inputBasico" name="senha">
+                </div>
+                <div class="input-holder">
+                  <div class="checkbox-holder">
+                    <input type="checkbox" class="inputBasicoCheck" name="cookie">
+                  </div>
+                  <div class="remember-holder">
+                    Manter-se conectado
+                  </div>
+                </div>
+
+
+              </div>
+              <div class="chapter-extremidade">
+                <button>Registrar-se</button> <button type="submit">Entrar</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+        <div class="container-erro" id="footer-holder">
+          <?php 
+            echo $_SESSION["mensagem"];
+          ?>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 est-10">
+
+      </div>
+    </div>
+  </div>
 
   <script src="../../assets/js/errosInsanos.js"></script>
 
--- finale/assets/js/errosInsanos.js
+++ finale/assets/js/errosInsanos.js
@@ -1,14 +1,13 @@
 let formulario = document.getElementById("oNormal");
 let footer = document.getElementById("footer-holder");
 let campo = document.getElementById("errado");
-let feedback = document.getElementById("sumire");
 
-if (campo.value == 1) {
-    feedback.style.display = "none";
-    footer.style.height = "75px";
-    formulario.style.height = "815px";
+if (campo.value == 0) {
+    footer.style.display = "flex";
+    footer.style.height = "20%";
+    formulario.style.height = "80%";
 } else {
-    feedback.style.display = "flex";
-    footer.style.height = "275px";
-    formulario.style.height = "615px";
+    footer.style.display = "none";
+    footer.style.height = "0%";
+    formulario.style.height = "100%";
 }

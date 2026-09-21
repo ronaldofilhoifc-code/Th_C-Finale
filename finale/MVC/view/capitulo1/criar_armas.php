bf727a1754717658ce3447133ff980c46e3f2d3d
Gustavo Ruckl Pscheidt <gustavo.315770@sbs.ifc.edu.br>
2026-09-21 09:16:40 -0300

    parcialmente feito

---
+++ finale/MVC/view/capitulo1/criar_armas.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+  <title>Capitulo 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Inserção do Bootstrap versão 4 -->
+  <link rel="stylesheet" href="../../../assets/bootstrap/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="../../../assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Fim insercão Bootstrap Versão 4-->
+  <!-- css do site-->
+  <!-- <link href="../../../assets/css/style.css" rel="stylesheet">
+  <link href="../../../assets/css/paginaEstatica.css" rel="stylesheet">
+  <link href="../../../assets/css/capitulo1.css" rel="stylesheet"> -->
+  <link href="../../../assets/css/criar_armas.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+
+
+
+</div>
+
+
+
+
+</body>
+
+</html>
---
+++ finale/MVC/view/capitulo1/tabela_armas.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+  <title>Capitulo 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Inserção do Bootstrap versão 4 -->
+  <link rel="stylesheet" href="../../../assets/bootstrap/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="../../../assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Fim insercão Bootstrap Versão 4-->
+  <!-- css do site-->
+  <!-- <link href="../../../assets/css/style.css" rel="stylesheet">
+  <link href="../../../assets/css/paginaEstatica.css" rel="stylesheet">
+  <link href="../../../assets/css/capitulo1.css" rel="stylesheet"> -->
+  <link href="../../../assets/css/tabela_armas.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="table-holder">
+    <table id="tabelao">
+      <thead>
+        <th>id</th>
+        <th>porte</th><!-- grande, pequeno, etc -->
+        <th>raridade</th>
+        <th>potencial</th><!-- branca,de fogo-->
+        <th>quantidade</th>
+        <th>condicao</th><!-- desgastado, limpo, etc -->
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>aaaaa</td>
+          <td>aaaaa</td>
+          <td>aaaaa</td>
+          <td>aaaaa</td>
+          <td>aaaaa</td>
+          <td>aaaaa</td>
+
+
+        </tr>
+
+
+      </tbody>
+
+    </table>
+
+  </div>
+
+  <div class="btn-holder">
+    <button onclick="window.location.href='./criar_armas.php'">Criar armas</button>
+  </div>
+
+
+</body>
+
+</html>
